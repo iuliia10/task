8f014359570e0f79261101be3b44c09f2f6980cf
iuliia10 <iuliiasvietova@yahoo.co.uk>
2025-10-24 12:59:12 +0100

    Fix PHP syntax and improve output status message

--- apitask/libs/php/getWeather.php
+++ apitask/libs/php/getWeather.php
@@ -24,7 +24,7 @@
 	$decode = json_decode($result,true);	
 
 	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
+	$output['status']['name'] = "success";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
 	$output['data'] = $decode['weatherObservations']??[];
@@ -34,6 +34,7 @@
 	echo json_encode($output); 
 
 ?>
+
 
 
 
