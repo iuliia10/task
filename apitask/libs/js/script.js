416f0719d5139adb87deb28aaa0bda91ea1056b3
iuliia10 <iuliiasvietova@yahoo.co.uk>
2025-10-24 10:51:33 +0100

    api

---
+++ apitask/libs/js/script.js
@@ -0,0 +1,83 @@
+
+$('#btnTz').click(function() {
+
+  $.ajax({
+    url: "libs/php/getTimeZone.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      lat: $('#tzLat').val(),
+      lng: $('#tzLng').val()
+    },
+    success: function(result) {
+      console.log(JSON.stringify(result));
+
+      if (result.status.name == "success") {
+        $('#txtCountry').html(result['data']['countryName']);
+        $('#txtTime').html(result['data']['time']);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+
+    }
+  });
+
+});
+
+
+
+
+$('#btnWeather').click(function() { 
+  $.ajax({
+    url: "libs/php/getWeather.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      lat: $('#wLat').val(),
+      lng: $('#wLng').val()
+    },
+    success: function(result) {
+      console.log(JSON.stringify(result));
+
+      if (result.status.name == "ok") {
+        $('#txtTemp').html(result['data']['weatherObservation']['temperature']);
+        $('#txtStation').html(result['data']['weatherObservation']['stationName']);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      
+    }
+  }); 
+});
+
+
+
+
+
+
+$('#btnEarth').click(function() {
+
+  $.ajax({
+    url: "libs/php/getEarthquakes.php",
+    type: 'POST',
+    dataType: 'json',
+    data: {
+      north: $('#eqNorth').val(),
+      south: $('#eqSouth').val(),
+      east: $('#eqEast').val(),
+      west: $('#eqWest').val()
+    },
+    success: function(result) {
+      console.log(JSON.stringify(result));
+
+      if (result.status.name == "ok") {
+        
+        $('#txtMag').html(result['data'][0]['magnitude']);
+      }
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      
+    }
+  }); 
+
+});
